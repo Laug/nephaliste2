9ffc26645e133e4e829d4172ec4d05a195e4b30b
Bruno Cauet <brunocauet@gmail.com>
2013-03-09 18:30:39 +0100

    Consumer: richer experience

--- consumer/models.py
+++ consumer/models.py
@@ -8,6 +8,15 @@ class Consumer(User):
 	user = OneToOneField(User)
 	surnom = CharField(max_length=50, blank=True)
 	solde = DecimalField(max_digits=5, decimal_places=2, default=0)
-	caution = DateField(blank=True, null=True)
+	caution = BooleanField()
 
 	promotion = IntegerField(blank=True, null=True)
+
+	def __unicode__(self):
+		return __unicode__(user)
+
+	def can_afford(self, amount):
+		return ((not self.user.is_active)
+				and ((self.user.is_staff)
+					or (self.user.solde - amount > -25 and self.caution)
+					or (self.user.solde - amount > 0)))
